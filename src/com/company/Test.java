0aa0486e7d3658f9881d379b69942d6265fc657e
dengjianying123 <dengjianying_xny@163.com>
2019-10-21 10:33:19 +0800

    testOne

---
+++ src/com/company/Test.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by Administrator on 2019/10/21 0021.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("执行111");
+        System.out.println("执行22");
+    }
+}
